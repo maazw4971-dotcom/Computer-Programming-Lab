0b7fe51c81c67cf934f0e9479dfde9da611016b5
maazw4971-dotcom <maazw4971@gmail.com>
2026-04-21 10:14:15 +0500

    Add files via upload

---
+++ LAB 02/LAB WORK/LAB_2 LabWork_3.cpp
@@ -0,0 +1,23 @@
+// Name: maaz wahab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0719
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
+
