0de92df5fa4eb856aeba3e4404bab4b3b395b725
maazw4971-dotcom <maazw4971@gmail.com>
2026-04-21 10:16:20 +0500

    Add files via upload

---
+++ LAB 02/LAB TASK/LAB_2 TASK_5.cpp
@@ -0,0 +1,26 @@
+// Name: maaz wahab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0719
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+    cout << " maaz wahab" << endl;
+
+    return 0;  // program ends
+}
+
+
---
+++ LAB 02/LAB TASK/LAB_2 TASK_7.cpp
@@ -0,0 +1,20 @@
+// Name: maaz wahab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0719
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << " maaz wahab " << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
+
+
