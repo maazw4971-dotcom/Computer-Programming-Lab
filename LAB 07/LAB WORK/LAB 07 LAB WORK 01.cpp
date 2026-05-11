21f6bc46508b577416f6cd7c0187bfe7df00ae48
maazw4971-dotcom <maazw4971@gmail.com>
2026-05-11 16:16:35 +0500

    Add files via upload

---
+++ LAB 07/LAB WORK/LAB 07 LAB WORK 01.cpp
@@ -0,0 +1,23 @@
+//NAME : MAAZ WAHAB
+//REG ID : BF25NWELE0719
+//DEPARTMENT: ELECTRICAL ENGINERRING POWER 
+//SECTION : A 
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declare an integer array of size 5
+    int numbers[5] = {10, 20, 30, 40, 50};
+
+    // Display array elements using a loop
+    cout << "Array elements are: " << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        // Print each element using index
+        cout << numbers[i] << endl;
+    }
+
+    return 0;
+}
