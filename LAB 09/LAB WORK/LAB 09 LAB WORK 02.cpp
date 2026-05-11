97d4fd6d7163014bb629e20f502bb0ab533b5ac0
maazw4971-dotcom <maazw4971@gmail.com>
2026-05-11 21:54:41 +0500

    Add files via upload

---
+++ LAB 09/LAB WORK/LAB 09 LAB WORK 02.cpp
@@ -0,0 +1,24 @@
+//NAME : MAAZ WAHAB
+//REG ID : BF25NWELE0719
+//DEPARTMENT : ELECTRICAL ENGINEERING
+//SECTION : A
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void sayHello();
+
+int main()
+{
+    // Calling the function
+    sayHello();
+
+    return 0;
+}
+
+// Function definition
+void sayHello()
+{
+    // Displaying Hello World message
+    cout << "Hello, World!" << endl;
+}
